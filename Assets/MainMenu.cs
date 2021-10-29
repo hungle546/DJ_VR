caed6f005613d909757bf31c3e579a29f86fce82
Ethan Lo <48305607+ethanlo100@users.noreply.github.com>
2021-10-29 08:19:06 -0700

    update to MainMenu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,8 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource buttonSound1;
+
+    //NOTE: XR Hand Controllers can only be one or the other: direct interaction or ray interaction
+    //For MainMenu, left and right controllers are ray interaction.
+    //In DJ Scene, left and right controllers are direct interaction.
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ButtonHoverSound()
+    {
+        buttonSound1.Play();
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
     }
 }
